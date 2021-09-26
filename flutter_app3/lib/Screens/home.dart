c59df2c5982bcc36d019118faf4465cc004166e8
UssrFantom <dimkakrohin@mail.ru>
2021-09-26 16:50:15 +0300

    made a to-do list

---
+++ flutter_app3/lib/Screens/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+List todoList = [];
+String _userToDo = "";
+
+
+
+
+@override
+  void initState() {
+    super.initState();
+    todoList.addAll(['Купить молоко', 'Сьесть арбуз', 'Проверить почту']);
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('список дел'),
+            centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext context, int index){
+            return Dismissible(
+                key: Key(todoList[index]),
+                child: Card(
+                  child: ListTile(
+                      title: Text(todoList[index]),
+                    trailing: IconButton(
+                      icon: Icon(
+                          Icons.delete,
+                          color: Colors.deepOrangeAccent,
+                    ),
+                    onPressed: (){
+                      setState(() {
+                        todoList.removeAt(index);
+                      });
+                    },
+                  ),
+                ),
+                ),
+
+                onDismissed: (direction){
+                  //if (direction == DismissDirection.)
+                  setState(() {
+                    todoList.removeAt(index);
+                  });
+            },
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        onPressed: () {
+          showDialog(context: context, builder: (BuildContext context){
+            return AlertDialog(
+              title: Text('Добавить эдемент'),
+              content: TextField(
+                onChanged: (String value){
+                    _userToDo = value;
+                },
+              ),
+              actions: [
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    todoList.add(_userToDo);
+                  });
+                  Navigator.of(context).pop();
+                }, child: Text('Добавить'))
+              ],
+            );
+          });
+      },
+        child: Icon(
+          Icons.add_box,
+          color: Colors.white
+        ),
+      ),
+    );
+  }
+}
